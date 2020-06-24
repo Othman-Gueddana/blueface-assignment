b98588622ad8724a066a08bd08dba98ab4a504ea
Debmallya Bhattacharya <batbrain9392@gmail.com>
2020-06-24 12:32:24 +0100

    fetch user until received + show loading + disable inputs until result

--- src/app/profile-settings/profile-settings.component.ts
+++ src/app/profile-settings/profile-settings.component.ts
@@ -9,10 +9,29 @@ import { IProfile, ProfileService } from '../profile-service/profile.service'
 export class ProfileSettingsComponent implements OnInit {
   public title = 'Profile'
   public user: IProfile
+  public isLoading: boolean
 
   constructor(private profile: ProfileService) {}
 
-  ngOnInit() {}
+  private async getProfile() {
+    let tryNum = 0
+    while (!this.user) {
+      tryNum++
+      console.log('try', tryNum)
+      try {
+        this.user = await this.profile.getProfileUser()
+      } catch (error) {
+        console.log(error.error)
+      }
+    }
+    console.log('success')
+  }
+
+  async ngOnInit() {
+    this.isLoading = true
+    await this.getProfile()
+    this.isLoading = false
+  }
 
   saveProfile() {}
 }
