e70e0081082865b1325d51e47eadb6d40ba95196
Debmallya Bhattacharya <batbrain9392@gmail.com>
2020-06-24 12:15:21 +0100

    project setup according to assignment

---
+++ src/app/profile-settings/profile-settings.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core'
+import { IProfile, ProfileService } from '../profile-service/profile.service'
+
+@Component({
+  selector: 'app-profile-settings',
+  templateUrl: './profile-settings.component.html',
+  styleUrls: ['./profile-settings.component.css'],
+})
+export class ProfileSettingsComponent implements OnInit {
+  public title = 'Profile'
+  public user: IProfile
+
+  constructor(private profile: ProfileService) {}
+
+  ngOnInit() {}
+
+  saveProfile() {}
+}
