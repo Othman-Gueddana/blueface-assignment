fee462772ab6a9a34801569157e7cdbb89ee462b
Debmallya Bhattacharya <batbrain9392@gmail.com>
2020-06-24 17:02:03 +0100

    header added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  template: `<app-profile-settings></app-profile-settings>`,
+  template: `
+    <div class="lang">
+      <a href="https://ng-i18n.web.app/">English</a>
+      <a href="https://fr-ng-i18n.web.app/">French</a>
+      <a href="https://es-ng-i18n.web.app/">Spanish</a>
+    </div>
+    <app-profile-settings></app-profile-settings>
+  `,
 })
 export class AppComponent {}
